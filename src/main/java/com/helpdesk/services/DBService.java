1a4bc187e449a7d8dba764ca2925efe025a9ccce
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-08-18 20:42:47 -0300

    Perfil de test criado: Para carregar dados ficticios para o H2

---
+++ src/main/java/com/helpdesk/services/DBService.java
@@ -0,0 +1,41 @@
+package com.helpdesk.services;
+
+import com.helpdesk.domain.Chamado;
+import com.helpdesk.domain.Cliente;
+import com.helpdesk.domain.Tecnico;
+import com.helpdesk.domain.enums.Perfil;
+import com.helpdesk.domain.enums.Prioridade;
+import com.helpdesk.domain.enums.Status;
+import com.helpdesk.repositories.ChamadoRepository;
+import com.helpdesk.repositories.ClienteRepository;
+import com.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    public void instanciaDB(){
+        Tecnico tec1 = new Tecnico(null, "Aldo Pereira", "01234567891", "aldo@email.com", "123");
+        tec1.addPerfil(Perfil.ADMIN);
+
+        Cliente cli1 = new Cliente(null,"Linus Torvalds", "98765432101", "linus@email.com","1234");
+
+        Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado-01", "Primeiro chamado", tec1, cli1);
+
+        tecnicoRepository.saveAll(Arrays.asList(tec1));
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        chamadoRepository.saveAll(Arrays.asList(c1));
+    }
+}
