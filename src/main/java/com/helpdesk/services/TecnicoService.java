c53f24f53ef29d9a8950f4e5df3ee588a71846f7
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-08-19 11:06:25 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/helpdesk/services/TecnicoService.java
+++ src/main/java/com/helpdesk/services/TecnicoService.java
@@ -2,6 +2,7 @@ package com.helpdesk.services;
 
 import com.helpdesk.domain.Tecnico;
 import com.helpdesk.repositories.TecnicoRepository;
+import com.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,6 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow( () -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
     }
 }
