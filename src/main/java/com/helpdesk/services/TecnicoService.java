a6e85e70134bda0c16c8621eb8f8afa4d6866b0c
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-08-19 11:24:22 -0300

    Criação do Endpoint para buscar todos os tecnicos

--- src/main/java/com/helpdesk/services/TecnicoService.java
+++ src/main/java/com/helpdesk/services/TecnicoService.java
@@ -6,6 +6,7 @@ import com.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,5 +17,9 @@ public class TecnicoService {
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
         return obj.orElseThrow( () -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+    }
+
+    public List<Tecnico> findAll() {
+        return repository.findAll();
     }
 }
