76792db8bca508d514559d88367f3f1b339b7a57
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-08-21 19:46:28 -0300

    Classes: Chamados DTO, Chamados Resources, ChamadoService e Criação do método find by ID

---
+++ src/main/java/com/helpdesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.helpdesk.services;
+
+
+import com.helpdesk.domain.Chamado;
+import com.helpdesk.repositories.ChamadoRepository;
+import com.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    public Chamado findById(Integer id){
+        Optional<Chamado> obj =chamadoRepository.findById(id);
+        return obj.orElseThrow( () -> new ObjectNotFoundException("Objeto não encontrado! ID" + id));
+    }
+}
