7ea4862da24f72a3e0c24f4634af4934d7616a98
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-08-25 15:18:37 -0300

    Liberar acesso ao H2

--- src/main/java/com/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/helpdesk/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.helpdesk.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,9 +15,19 @@ import java.util.Arrays;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final  String[] PUBLIC_MATCHERS = {"/h2-console/**"};
+
+    @Autowired
+    private Environment env;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
+        http.authorizeRequests()
+                        .antMatchers(PUBLIC_MATCHERS).permitAll()
+                        .anyRequest().authenticated();
         http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
