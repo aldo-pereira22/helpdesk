9766ed5ff19e5a70c8dbe8751689aa80eab6bb85
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-08-26 09:06:04 -0300

    Criação do construtor para os atributos da Calsse UserSS

--- src/main/java/com/helpdesk/security/UserSS.java
+++ src/main/java/com/helpdesk/security/UserSS.java
@@ -1,25 +1,49 @@
 package com.helpdesk.security;
 
+import com.helpdesk.domain.enums.Perfil;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class UserSS implements UserDetails {
+    private static final long serialVersionUID = 1L;
+     
+    private Integer id;
+    private String email;
+    private String senha;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSS(Integer id, String email, String senha, Set<Perfil> perfis) {
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+        this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao()))
+                .collect(Collectors.toSet());
+        ;
+    }
+
+    public Integer getId() {
+        return id;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return senha;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
