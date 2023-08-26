f993aec9fee1855094c5f001a64a4b471cef6110
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-08-26 09:39:44 -0300

    Classe JWTAuthenticanFilter, método de tentativa de Login

---
+++ src/main/java/com/helpdesk/security/JWTAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.helpdesk.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.helpdesk.domain.dtos.CredenciaisDTO;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+    private JWTUtil jwtUtil;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+        super();
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+
+       try {
+           CredenciaisDTO creds = new ObjectMapper().readValue(request.getInputStream(), CredenciaisDTO.class);
+           UsernamePasswordAuthenticationToken authenticationToken = new
+                   UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getSenha(), new ArrayList<>());
+           Authentication authentication = authenticationManager.authenticate(authenticationToken);
+           return authentication;
+       }catch (Exception e){
+           throw new RuntimeException(e);
+       }
+
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult)
+            throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed)
+            throws IOException, ServletException {
+        super.unsuccessfulAuthentication(request, response, failed);
+    }
+}
+
+
+
