fc57b6ca48e5040a8890236e94bae75d5b552c0d
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-08-26 09:16:18 -0300

    Classe Service UserDetails para buscar um usuário pelo email

--- src/main/java/com/helpdesk/security/UserSS.java
+++ src/main/java/com/helpdesk/security/UserSS.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 
 public class UserSS implements UserDetails {
     private static final long serialVersionUID = 1L;
-     
+
     private Integer id;
     private String email;
     private String senha;
