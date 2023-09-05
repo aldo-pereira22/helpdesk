e6cc95eb6b8af86495bfd38d4aef3ca933116541
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-09-05 10:55:04 -0300

    Componente técnico-update

--- helpdesk-front/src/app/app.module.ts
+++ helpdesk-front/src/app/app.module.ts
@@ -40,6 +40,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { NgxMaskModule } from 'ngx-mask';
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 @NgModule({
 
@@ -50,7 +51,8 @@ import { NgxMaskModule } from 'ngx-mask';
     HeaderComponent,
     TecnicoListComponent,
     LoginComponent,
-    TecnicoCreateComponent
+    TecnicoCreateComponent,
+    TecnicoUpdateComponent
   ],
 
   imports: [
