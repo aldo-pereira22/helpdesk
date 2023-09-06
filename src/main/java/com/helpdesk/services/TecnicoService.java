257b2c95aa4030afe64e6453cc1b02387466565f
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-09-06 14:41:10 -0300

    Resolvendo BUG de não atualizar a senha

--- src/main/java/com/helpdesk/services/TecnicoService.java
+++ src/main/java/com/helpdesk/services/TecnicoService.java
@@ -45,6 +45,10 @@ public class TecnicoService {
     public Tecnico update(Integer id, TecnicoDTO objDto) {
         objDto.setId(id);
         Tecnico oldObj = findById(id);
+
+        if(!objDto.getSenha().equals(oldObj.getSenha())){
+            objDto.setSenha(encoder.encode(objDto.getSenha()));
+        }
         validaPorCpfeEmail(objDto);
         oldObj = new Tecnico(objDto);
         return repository.save(oldObj);
