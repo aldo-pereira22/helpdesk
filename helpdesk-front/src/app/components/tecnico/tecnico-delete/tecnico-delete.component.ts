d38e9338a183a1494ea8f64611e4fec47b52426e
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-09-06 15:11:13 -0300

    Técnico-DELETE finalizado

--- helpdesk-front/src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
+++ helpdesk-front/src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-delete',
@@ -7,9 +14,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoDeleteComponent implements OnInit {
 
-  constructor() { }
+  tecnico: Tecnico = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+
+  }
+
+
+
+  constructor(
+    private service: TecnicoService,
+    private toast: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.tecnico.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+    console.log(this.tecnico)
   }
+
+
+  findById() {
+    this.service.findById(this.tecnico.id).subscribe(resposta => {
+      resposta.perfis = []
+      this.tecnico = resposta
+      console.log(resposta.senha)
+    })
+  }
+
+  delete() {
+    this.service.delete(this.tecnico.id).subscribe(() => {
+      this.toast.success("Técnico Deletado com sucesso!", 'DELETE');
+      this.router.navigate(['tecnicos'])
+    }, ex => {
+      console.log(ex)
+      if (ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message)
+        });
+      } else {
+        this.toast.error(ex.error.message)
+      }
+    })
+  }
+
+
+
 
 }
