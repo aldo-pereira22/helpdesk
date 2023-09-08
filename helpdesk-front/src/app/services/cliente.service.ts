4d21a35ce2019dde9e00ee6afa89a288a657dc75
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-09-08 15:10:16 -0300

    Componentes de Criar, Atualizar, Editar e deletar cliente

---
+++ helpdesk-front/src/app/services/cliente.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from '../config/api.config';
+import { Cliente } from '../models/cliente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  constructor(private http: HttpClient) { }
+
+  findById(id: any): Observable<Cliente> {
+    return this.http.get<Cliente>(`${API_CONFIG.baseUrl}/tecnicos/${id}`)
+  }
+  findAll(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(`${API_CONFIG.baseUrl}/tecnicos`)
+  }
+
+  create(tecnico: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(`${API_CONFIG.baseUrl}/tecnicos`, tecnico);
+  }
+
+  update(tecnico: Cliente): Observable<Cliente> {
+    return this.http.put<Cliente>(`${API_CONFIG.baseUrl}/tecnicos/${tecnico.id}`, tecnico);
+  }
+  delete(id: any): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${API_CONFIG.baseUrl}/tecnicos/${id}`)
+  }
+}
