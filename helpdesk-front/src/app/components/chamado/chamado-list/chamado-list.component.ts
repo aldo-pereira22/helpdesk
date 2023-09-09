2a00d0a64e67d85dc2490885f0a1ad8c6bd33da0
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-09-09 15:25:42 -0300

    Filtro para chamados em ABERTO, EM ANDAMENTO e ENCERRADO

--- helpdesk-front/src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ helpdesk-front/src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -12,6 +12,7 @@ import { ChamadoService } from 'src/app/services/chamado.service';
 export class ChamadoListComponent implements OnInit {
 
   ELEMENT_DATA: Chamado[] = []
+  FILTERED_DATA: Chamado[] = []
 
   displayedColumns: string[] = ["id", "titulo", "cliente", "tecnico", "dataAbertura", "prioridade", "status", "acoes"]
   dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
@@ -28,7 +29,7 @@ export class ChamadoListComponent implements OnInit {
   findAll(): void {
     this.service.findAll().subscribe(resposta => {
       this.ELEMENT_DATA = resposta;
-      this.dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+      this.dataSource = new MatTableDataSource<Chamado>(resposta);
       this.dataSource.paginator = this.paginator;
     })
   }
@@ -55,5 +56,20 @@ export class ChamadoListComponent implements OnInit {
     } else {
       return 'ALTA'
     }
+  }
+
+  orderByStatus(status: any): void {
+    let list: Chamado[] = [];
+    this.ELEMENT_DATA.forEach(element => {
+      if (element.status == status) {
+        list.push(element)
+
+      }
+    })
+
+    this.FILTERED_DATA = list;
+    this.dataSource = new MatTableDataSource<Chamado>(list);
+    this.dataSource.paginator = this.paginator;
+
   }
 }
